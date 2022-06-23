5cd17d374f88f6247a79efbfb334b2f531eb623c
garbhagudi <garbhagudi.11@gmail.com>
2022-06-23 14:22:44 +0530

    Added Meta Pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,7 @@ function MyApp({ Component, pageProps }) {
     import("react-facebook-pixel")
       .then((x) => x.default)
       .then((ReactPixel) => {
-        ReactPixel.init("2326481447668134");
+        ReactPixel.init("714505339773431");
         ReactPixel.pageView();
         router.events.on("routeChangeComplete", () => {
           ReactPixel.pageView();
