ce7a57706f16491bbdc8366141e58525a2c75d5c
garbhagudi <garbhagudi.11@gmail.com>
2022-08-11 13:29:03 +0530

    gg spelling fix

--- src/sections/home/band.tsx
+++ src/sections/home/band.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Band = () => {
   return (
-    <div className="max-w-7xl mx-auto px-3 my-4 sm:px-6 lg:px-4 border-b border-gray-300 ">
-      <span className="text-xl md:text-2xl block text-center mx-auto font-benne font-bold">
+    <div className="px-3 mx-auto my-4 border-b border-gray-300 max-w-7xl sm:px-6 lg:px-4 ">
+      <span className="block mx-auto text-xl font-bold text-center md:text-2xl font-benne">
         <span className="block md:inline-block">
           <span className="text-brandPink">ಬಾಳಿಗೊಂದು ಬೇಕು </span>{" "}
           <span className="text-brandPurpleDark"> ಕರುಳಿನಾ ಕುಡಿ </span>{" "}
         </span>{" "}
         <span className="block md:inline-block">
           <span className="text-brandPink">ನಿಮ್ಮ ಕನಸನ್ನು ನನಸಾಗಿಸುತ್ತದೆ </span>{" "}
-          <span className="text-brandPurpleDark"> ಗಭರ್ಗುಡಿ </span>
+          <span className="text-brandPurpleDark"> ಗರ್ಭಗುಡಿ </span>
         </span>
       </span>
     </div>
