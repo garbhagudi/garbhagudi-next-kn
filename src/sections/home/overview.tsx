eeb19e4d5626bd0546e640cb048a61fc92a58a59
Inqonsecuential <theavinashmishra13@gmail.com>
2023-09-20 18:19:11 +0530

    minor changes

--- src/sections/home/overview.tsx
+++ src/sections/home/overview.tsx
@@ -1,70 +1,70 @@
-import React from "react";
+import React from 'react';
 
 const Overview = () => {
   return (
-    <div className="relative bg-white">
-      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start mt-10 md:mt-0">
-        <div className="relative sm:py-16 lg:py-0">
+    <div className='relative bg-white'>
+      <div className='lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start mt-10 md:mt-0'>
+        <div className='relative sm:py-16 lg:py-0'>
           <div
-            aria-hidden="true"
-            className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen"
+            aria-hidden='true'
+            className='hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen'
           >
-            <div className="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72" />
+            <div className='absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72' />
             <svg
-              className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
+              className='absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12'
               width={404}
               height={392}
-              fill="none"
-              viewBox="0 0 404 392"
+              fill='none'
+              viewBox='0 0 404 392'
             >
               <defs>
                 <pattern
-                  id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
+                  id='02f20b47-fd69-4224-a62a-4c9de5c763f7'
                   x={0}
                   y={0}
                   width={20}
                   height={20}
-                  patternUnits="userSpaceOnUse"
+                  patternUnits='userSpaceOnUse'
                 >
                   <rect
                     x={0}
                     y={0}
                     width={4}
                     height={4}
-                    className="text-gray-200"
-                    fill="currentColor"
+                    className='text-gray-200'
+                    fill='currentColor'
                   />
                 </pattern>
               </defs>
               <rect
                 width={404}
                 height={392}
-                fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)"
+                fill='url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)'
               />
             </svg>
           </div>
-          <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
+          <div className='relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20'>
             {/* Testimonial card*/}
-            <div className="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
+            <div className='relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden'>
               <img
-                className="absolute inset-0 h-full w-full object-cover"
-                src="https://res.cloudinary.com/garbhagudiivf/image/upload/v1643273193/Home/Why_GarbhaGudi_oa1nv7.webp"
-                alt=""
+                className='absolute inset-0 h-full w-full object-cover'
+                src='https://res.cloudinary.com/garbhagudiivf/image/upload/v1670926732/Home/Why_GarbhaGudi_oa1nv7-min_z7p9jv.jpg'
+                alt=''
               />
-              <div className="absolute inset-0 bg-gray-200 mix-blend-multiply" />
-              <div className="absolute inset-0 bg-gradient-to-t from-brandPink via-gray-400 mix-blend-multiply" />
-              <div className="relative px-8">
-                <blockquote className="mt-8">
-                  <div className="relative text-white md:flex-grow">
+              <div className='absolute inset-0 bg-gray-200 mix-blend-multiply' />
+              <div className='absolute inset-0 bg-gradient-to-t from-brandPink via-gray-400 mix-blend-multiply' />
+              <div className='relative px-8'>
+                <blockquote className='mt-8'>
+                  <div className='relative text-white md:flex-grow'>
                     <svg
-                      className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-brandPink"
-                      fill="currentColor"
-                      viewBox="0 0 32 32"
-                      aria-hidden="true"
+                      className='absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-brandPink'
+                      fill='currentColor'
+                      viewBox='0 0 32 32'
+                      aria-hidden='true'
                     >
-                      <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                      <path d='M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z' />
                     </svg>
-                    <p className="relative text-md font-benne">
+                    <p className='relative text-md font-benne'>
                       7000ಕ್ಕೂ ಹೆಚ್ಚಿನ ಸಂಖ್ಯೆಯ ದಂಪತಿಗಳು ತಮ್ಮದೇ ಆದ ಜೈವಿಕ
                       ಶಿಶುವನ್ನು ಹೊಂದುವುದರ ಖಾತ್ರಿ ಮಾಡಿಕೊಳ್ಳುವ ಮೂಲಕ ಅವರ ಜೀವನದಲ್ಲಿ
                       ಸುಖಾಂತ್ಯವನ್ನು ಸೃಷ್ಟಿಸುವಲ್ಲಿ ಕೇಂದ್ರವು ಪ್ರಮುಖ ಪಾತ್ರ ವಹಿಸಿದೆ.
@@ -77,8 +77,8 @@ const Overview = () => {
                     </p>
                   </div>
 
-                  <footer className="mt-4">
-                    <p className="text-lg font-semibold text-purple-300 font-content">
+                  <footer className='mt-4'>
+                    <p className='text-lg font-semibold text-purple-300 font-content'>
                       ಡಾ ಆಶಾ ಎಸ್. ವಿಜಯ್, ಗರ್ಭಗುಡಿಯ ಸಂಸ್ಥಾಪಕರು ಮತ್ತು ವೈದ್ಯಕೀಯ
                       ನಿರ್ದೇಶಕರು
                     </p>
@@ -89,12 +89,12 @@ const Overview = () => {
           </div>
         </div>
 
-        <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
-          <div className="pt-12 sm:pt-16 lg:pt-20">
-            <h2 className="text-3xl text-brandPink sm:text-4xl font-heading font-semibold lg:mt-10">
+        <div className='relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0'>
+          <div className='pt-12 sm:pt-16 lg:pt-20'>
+            <h2 className='text-3xl text-brandPink sm:text-4xl font-heading font-semibold lg:mt-10'>
               ಗರ್ಭಗುಡಿ ಏಕೆ?
             </h2>
-            <div className="text-brandDark sm:mt-5 text-md sm:text-lg font-content">
+            <div className='text-brandDark sm:mt-5 text-md sm:text-lg font-content'>
               <p>
                 ಒಂದು ದಶಕದ ಅವಧಿಯಲ್ಲಿ ಸಂತಾನ ಪಡೆಯುವ ಆಸಕ್ತಿ ಹೊಂದಿರುವ ಸಾವಿರಾರು
                 ದಂಪತಿಗಳಿಗೆ ಆಶಾಕಿರಣವಾಗಿ ಗರ್ಭಗುಡಿ ಸೇವೆ ನೀಡಿದೆ. ನಿಮ್ಮ ಅಗತ್ಯಗಳನ್ನು
